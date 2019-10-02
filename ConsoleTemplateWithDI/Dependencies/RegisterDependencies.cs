d550c427cd925280fd4be0fa855a08145865056b
Kenely de Oliveira <kenely.oliveira@gmail.com>
2019-10-02 18:43:16 +0100

    Create base for Dependency Injection

---
+++ ConsoleTemplateWithDI/Dependencies/RegisterDependencies.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleTemplateWithDI.Dependencies
+{
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+
+    internal static class RegisterDependencies
+    {
+        internal static void Register()
+        {
+            IServiceProvider serviceProvider = new ServiceCollection()
+                //.AddSingleton<IChristmasDiscountVisitor, ChristmasDiscountVisitor>()
+                //.AddSingleton<IMothersDayDiscountVisitor, MothersDayDiscountVisitor>()
+                //.AddSingleton<IDoSomethingElseVisitor, DoSomethingElseVisitor>()
+                //.AddSingleton<IVisitor>(s => s.GetRequiredService<IChristmasDiscountVisitor>())
+                //.AddSingleton<IVisitor>(s => s.GetRequiredService<IMothersDayDiscountVisitor>())
+                //.AddSingleton<IVisitor>(s => s.GetRequiredService<IDoSomethingElseVisitor>())
+                .BuildServiceProvider();
+        }
+    }
+}
